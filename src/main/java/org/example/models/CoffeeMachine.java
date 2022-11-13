d0fab00a46b456c0e024697eab456c649e8b7f7a
ncmprbll <ncmprbll@github.com>
2022-11-13 23:41:23 +0700

    Initial version

---
+++ src/main/java/org/example/models/CoffeeMachine.java
@@ -0,0 +1,78 @@
+package org.example.models;
+
+/**
+ * Coffee machine class
+ */
+public class CoffeeMachine {
+    /** ID */
+    private Integer id;
+    /** Brand of a coffee machine */
+    private String brand;
+    /** Model of a coffee machine */
+    private String model;
+    /** Country of origin of a coffee machine */
+    private String countryOfOrigin;
+    /** Price of a coffee machine */
+    private Double price;
+
+    public CoffeeMachine(Integer id, String brand, String model, String countryOfOrigin, Double price) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.countryOfOrigin = countryOfOrigin;
+        this.price = price;
+    }
+
+    public CoffeeMachine() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCountryOfOrigin() {
+        return countryOfOrigin;
+    }
+
+    public void setCountryOfOrigin(String countryOfOrigin) {
+        this.countryOfOrigin = countryOfOrigin;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "CoffeeMachine{" +
+                "id=" + id +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", countryOfOrigin='" + countryOfOrigin + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
