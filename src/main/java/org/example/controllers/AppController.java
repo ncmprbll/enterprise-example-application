58d6adcf7e1cb6f4a509eb8f2e7f0c0839ede93d
ncmprbll <ncmprbll@github.com>
2022-11-14 15:12:49 +0700

    Add, edit and delete pages

--- src/main/java/org/example/controllers/AppController.java
+++ src/main/java/org/example/controllers/AppController.java
@@ -1,8 +1,22 @@
 package org.example.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AppController {
+    @GetMapping("/add")
+    public String getAdd() {
+        return "add";
+    }
 
+    @GetMapping("/edit")
+    public String getEdit() {
+        return "edit";
+    }
+
+    @GetMapping("/delete")
+    public String getDelete() {
+        return "delete";
+    }
 }
